7bc0240818ca68a0740e77046f52f8b339a8cd62
teiatan <92535482+teiatan@users.noreply.github.com>
2022-09-30 17:27:14 +0300

    task1 is modified. task2 is done

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
 const ulCategoriesEl = document.querySelector(`#categories`);
 const allLiInCategoriesEl = ulCategoriesEl.querySelectorAll(`li.item`);
 console.log(`Number of categories: ${allLiInCategoriesEl.length}`);
-const titlesEl = document.querySelectorAll(`h2`);
+
 
 for (let category of allLiInCategoriesEl) {
     console.log(`Category: ${category.querySelector(`h2`).textContent}`);
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsContainerEl = document.querySelector(`#ingredients`);
+const ingredientsListEl = ingredients.map(ingredient => {
+  const ingredientEl = document.createElement(`li`);
+  ingredientEl.textContent = ingredient;
+  ingredientEl.classList.add(`item`);
+
+  return ingredientEl;
+})
+
+console.log(ingredientsListEl);
+ingredientsContainerEl.append(...ingredientsListEl);
