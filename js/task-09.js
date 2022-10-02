19f699628f8067e6e34efb4a7232d2a6c2de8b81
teiatan <92535482+teiatan@users.noreply.github.com>
2022-10-02 13:42:54 +0300

    task 9 is done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector(`.change-color`);
+const bodyEl = document.querySelector(`body`);
+const colorSpanEl = document.querySelector(`.color`);
+
+buttonEl.addEventListener(`click`, backgroundColorChangeHandler);
+
+function backgroundColorChangeHandler(event) {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorSpanEl.textContent = bodyEl.style.backgroundColor;
+};
