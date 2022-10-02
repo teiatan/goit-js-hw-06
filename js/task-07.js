e0ef7a973c1c1c7c68e42f9563146dfc02567107
teiatan <92535482+teiatan@users.noreply.github.com>
2022-10-02 15:51:20 +0300

    task 7 is done

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,7 @@ const inputEl = document.querySelector(`#font-size-control`);
 const textEl = document.querySelector(`#text`);
 
 inputEl.addEventListener(`input`, fontSizeControlHandler);
+
 function fontSizeControlHandler(event) {
-    textEl.fontSize = event.currentTargetValue;
+    textEl.setAttribute(`style`, `font-size: ${event.currentTarget.value}px`);
 };
