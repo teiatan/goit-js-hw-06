b8e0daa8564c38ba5bbd202e2445e78ecfbf6ce4
teiatan <92535482+teiatan@users.noreply.github.com>
2022-10-02 13:11:30 +0300

    task6 is done

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const inputEl = document.querySelector(`#validation-input`);
+
+inputEl.addEventListener(`blur`, blurInputHandler);
+
+function blurInputHandler(event) {
+    if (event.currentTarget.value.length >= inputEl.dataset.length) {
+        inputEl.classList.add(`valid`);
+        inputEl.classList.remove(`invalid`);
+} else { 
+    inputEl.classList.add(`invalid`);
+    inputEl.classList.remove(`valid`);
+}
+
+};
