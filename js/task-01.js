97a7b45f5a1a6dde037825890f75a321aa99bbf4
teiatan <92535482+teiatan@users.noreply.github.com>
2022-11-09 12:48:34 +0200

    task 1 DOM navigation with fistElementChild etc

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,4 @@
-const ulCategoriesEl = document.querySelector(`#categories`);
+/* const ulCategoriesEl = document.querySelector(`#categories`);
 const allLiInCategoriesEl = ulCategoriesEl.querySelectorAll(`li.item`);
 console.log(`Number of categories: ${allLiInCategoriesEl.length}`);
 
@@ -7,3 +7,20 @@ for (let category of allLiInCategoriesEl) {
     console.log(`Category: ${category.querySelector(`h2`).textContent}`);
     console.log(`Elements: ${category.querySelectorAll(`li`).length}`);
 }
+ 
+ */
+
+const ulCategoriesEl = document.querySelector(`#categories`);
+
+console.log(`
+    Number of categories: ${ulCategoriesEl.children.length}
+
+    Category: ${ulCategoriesEl.firstElementChild.firstElementChild.textContent}
+    Elements: ${ulCategoriesEl.firstElementChild.lastElementChild.children.length}
+
+    Category: ${ulCategoriesEl.firstElementChild.nextElementSibling.firstElementChild.textContent}
+    Elements: ${ulCategoriesEl.firstElementChild.nextElementSibling.lastElementChild.children.length}
+
+    Category: ${ulCategoriesEl.lastElementChild.firstElementChild.textContent}
+    Elements: ${ulCategoriesEl.lastElementChild.lastElementChild.children.length}
+`);
