47cce49cde7da8b92c12730fab3a33f65c079aa3
teiatan <92535482+teiatan@users.noreply.github.com>
2022-12-30 19:15:45 +0200

    task 6: number of symbols ===6

--- js/task-06.js
+++ js/task-06.js
@@ -3,12 +3,11 @@ const inputEl = document.querySelector(`#validation-input`);
 inputEl.addEventListener(`blur`, blurInputHandler);
 
 function blurInputHandler(event) {
-    if (event.currentTarget.value.length >= inputEl.dataset.length) {
+    if (event.currentTarget.value.length === Number(inputEl.dataset.length)) {
         inputEl.classList.add(`valid`);
         inputEl.classList.remove(`invalid`);
-} else { 
-    inputEl.classList.add(`invalid`);
-    inputEl.classList.remove(`valid`);
-}
-
+    } else { 
+        inputEl.classList.add(`invalid`);
+        inputEl.classList.remove(`valid`);
+    }
 };
